f2b038d5f5b83d91e0a41c27fd9373b337f8fe84
Marcin <marcingorecki1981@gmail.com>
2020-07-04 13:14:12 +0200

    sample images introduced

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,1 +1,11 @@
-console.log('loaded')
+'use strict'
+const words = [
+	{ word: 'KOT', image: 'kot.png', letters: ['K', 'O', 'T'] },
+	{ word: 'DOM', image: 'dom.png', letters: ['D', 'O', 'M'] },
+	{ word: 'DYNIA', image: 'dynia.png', letters: ['D', 'Y', 'N', 'I', 'A'] },
+]
+
+const pictureBox = document.querySelector('.image-container')
+//wyświetl obraz
+//wyświetl przycisk ponownego odtwarzania słowa
+//Powiedz słowo
