11a808cf254370be148166d93ee732d5cd576ff6
Marcin <marcingorecki1981@gmail.com>
2020-07-04 15:39:10 +0200

    initial animations and speeech synthesis

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,11 +1,70 @@
 'use strict'
 const words = [
-	{ word: 'KOT', image: 'kot.png', letters: ['K', 'O', 'T'] },
-	{ word: 'DOM', image: 'dom.png', letters: ['D', 'O', 'M'] },
-	{ word: 'DYNIA', image: 'dynia.png', letters: ['D', 'Y', 'N', 'I', 'A'] },
+	{
+		word: 'KOT',
+		image: 'kot.png',
+		letters: ['K', 'O', 'T'],
+	},
+	{
+		word: 'DOM',
+		image: 'dom.png',
+		letters: ['D', 'O', 'M'],
+	},
+	{
+		word: 'DYNIA',
+		image: 'dynia.png',
+		letters: ['D', 'Y', 'N', 'I', 'A'],
+	},
 ]
-
-const pictureBox = document.querySelector('.image-container')
+const playWord = (nr) => {
+	var msg = new SpeechSynthesisUtterance(`${words[nr].word}`)
+	var voices = window.speechSynthesis.getVoices()
+	msg.voice = voices[0]
+	window.speechSynthesis.speak(msg)
+}
+const saySingleLetter = (letter) => {
+	var msg = new SpeechSynthesisUtterance(letter)
+	var voices = window.speechSynthesis.getVoices()
+	msg.voice = voices[0]
+	window.speechSynthesis.speak(msg)
+}
 //wyświetl obraz
-//wyświetl przycisk ponownego odtwarzania słowa
+const createPicture = (nr) => {
+	const pictureBox = document.querySelector('.image-container')
+	pictureBox.innerHTML = ''
+	pictureBox.style.backgroundImage = `url('./dist/images/${words[nr].image}')`
+	pictureBox.addEventListener('click', function () {
+		playWord(nr)
+	})
+}
+
+const createLetters = (nr) => {
+	const letterBox = document.querySelector('.letters')
+	letterBox.innerHTML = ''
+
+	let shuffledLetters = words[nr].letters
+		.map((a) => ({ sort: Math.random(), value: a }))
+		.sort((a, b) => a.sort - b.sort)
+		.map((a) => a.value)
+	shuffledLetters.forEach((letter) => {
+		let singleLetterContainer = document.createElement('p')
+		singleLetterContainer.textContent = letter
+		let color = Math.floor(Math.random() * 360) + 1
+		singleLetterContainer.style.color = `hsl(${color}, 100%, 50%)`
+		singleLetterContainer.classList.add('animate__animated')
+		singleLetterContainer.addEventListener('click', function () {
+			saySingleLetter(letter)
+		})
+		singleLetterContainer.addEventListener('mouseenter', function () {
+			this.classList.add('animate__bounce')
+		})
+		singleLetterContainer.addEventListener('mouseleave', function () {
+			this.classList.remove('animate__bounce')
+		})
+
+		letterBox.appendChild(singleLetterContainer)
+	})
+}
+createPicture(0)
+createLetters(0)
 //Powiedz słowo
